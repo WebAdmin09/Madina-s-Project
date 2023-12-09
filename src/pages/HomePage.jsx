f775168265f5bc80a786731324ccb193d27c65c9
WebAdmin09 <husanorifjonov9@gmail.com>
2023-12-09 06:56:45 +0500

    last commit

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,10 +1,20 @@
 import React, { Fragment, useState } from "react";
 import tree from "../assets/tree.png";
-import apple from "../assets/apple.png";
 import basket from "../assets/basket.png";
 import style from "./HomePage.module.css";
 import Quiz from "../components/Quiz/Quiz";
 import Game from "../components/Game/Game";
+
+import apple1 from "../assets/apple1.png";
+import apple2 from "../assets/apple2.png";
+import apple3 from "../assets/apple2.png";
+import apple4 from "../assets/apple2.png";
+import apple5 from "../assets/apple2.png";
+import apple6 from "../assets/apple2.png";
+import apple7 from "../assets/apple2.png";
+import apple8 from "../assets/apple2.png";
+import apple9 from "../assets/apple2.png";
+import apple10 from "../assets/apple10.png";
 
 const HomePage = () => {
   const options = [
@@ -74,36 +84,99 @@ const HomePage = () => {
       correct: 2,
     },
   ];
-
+  const appleImages = [
+    apple1,
+    apple2,
+    apple3,
+    apple4,
+    apple5,
+    apple6,
+    apple7,
+    apple8,
+    apple9,
+    apple10,
+  ];
   const [step, setStep] = useState(0);
   const question = options[step];
   const [correct, setCorrect] = useState(0);
+  const [correctAnswerGiven1, setCorrectAnswerGiven1] = useState(false);
+  const [correctAnswerGiven2, setCorrectAnswerGiven2] = useState(false);
+  const [correctAnswerGiven3, setCorrectAnswerGiven3] = useState(false);
+  const [correctAnswerGiven4, setCorrectAnswerGiven4] = useState(false);
+  const [correctAnswerGiven5, setCorrectAnswerGiven5] = useState(false);
+  const [correctAnswerGiven6, setCorrectAnswerGiven6] = useState(false);
+  const [correctAnswerGiven7, setCorrectAnswerGiven7] = useState(false);
+  const [correctAnswerGiven8, setCorrectAnswerGiven8] = useState(false);
+  const [correctAnswerGiven9, setCorrectAnswerGiven9] = useState(false);
+  const [correctAnswerGiven10, setCorrectAnswerGiven10] = useState(false);
 
   const onClickVariant = (index) => {
     setStep(step + 1);
 
     if (index === question.correct) {
       setCorrect(correct + 1);
-      
+      if (step === 0) {
+        setCorrectAnswerGiven1(true);
+      }
+      if (step === 1) {
+        setCorrectAnswerGiven2(true);
+      }
+      if (step === 2) {
+        setCorrectAnswerGiven3(true);
+      }
+      if (step === 3) {
+        setCorrectAnswerGiven4(true);
+      }
+      if (step === 4) {
+        setCorrectAnswerGiven5(true);
+      }
+      if (step === 5) {
+        setCorrectAnswerGiven6(true);
+      }
+      if (step === 6) {
+        setCorrectAnswerGiven7(true);
+      }
+      if (step === 7) {
+        setCorrectAnswerGiven8(true);
+      }
+      if (step === 8) {
+        setCorrectAnswerGiven9(true);
+      }
+      if (step === 9) {
+        setCorrectAnswerGiven10(true);
+      }
     }
   };
+
+  const appleElements = appleImages.map((appleImg, index) => (
+    <img
+      key={index}
+      className={`${style[`apple${index + 1}`]}
+       ${index === 0 && correctAnswerGiven1 ? style.changed1 : ""}
+      ${index === 1 && correctAnswerGiven2 ? style.changed2 : ""}
+      ${index === 2 && correctAnswerGiven3 ? style.changed3 : ""}
+      ${index === 3 && correctAnswerGiven4 ? style.changed4 : ""}
+      ${index === 4 && correctAnswerGiven5 ? style.changed5 : ""}
+      ${index === 5 && correctAnswerGiven6 ? style.changed6 : ""}
+      ${index === 6 && correctAnswerGiven7 ? style.changed7 : ""}
+      ${index === 7 && correctAnswerGiven8 ? style.changed8 : ""}
+      ${index === 8 && correctAnswerGiven9 ? style.changed9 : ""}
+      ${index === 9 && correctAnswerGiven10 ? style.changed10 : ""}
+      `}
+      src={appleImg}
+      alt="apple"
+    />
+  ));
+
   return (
     <Fragment>
       <div className={style["main__wrapper"]}>
         <img className={style["treeImg"]} src={tree} alt="tree" />
-        <img className={style["basketImg"]} src={basket} alt="basket" />
-        <div className={style["apples"]}>
-          <img className={style["apple1"]} src={apple} alt="apple" />
-          <img className={style["apple2"]} src={apple} alt="apple" />
-          <img className={style["apple3"]} src={apple} alt="apple" />
-          <img className={style["apple4"]} src={apple} alt="apple" />
-          <img className={style["apple5"]} src={apple} alt="apple" />
-          <img className={style["apple6"]} src={apple} alt="apple" />
-          <img className={style["apple7"]} src={apple} alt="apple" />
-          <img className={style["apple8"]} src={apple} alt="apple" />
-          <img className={style["apple9"]} src={apple} alt="apple" />
-          <img className={style["apple10"]} src={apple} alt="apple" />
+        <div className={style["basket__wrap"]}>
+          <img className={style["basketImg"]} src={basket} alt="basket" />
+          <span className={style["correct__num"]}>{correct}</span>
         </div>
+        <div className={style["apples"]}>{appleElements}</div>
       </div>
       {step !== options.length ? (
         <Quiz
